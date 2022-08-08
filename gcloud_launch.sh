6aca86f9613f787ca222cfa3b990065c64f8a4ec
jacobthebanana <jacob@banana.abay.cf>
2022-08-08 20:50:02 +0000

    Updated mount point name on sweep VMs

--- gcloud_launch.sh
+++ gcloud_launch.sh
@@ -7,17 +7,17 @@ gcloud compute disks create ${destination_disk_name} \
 
 
 for index in {01..30} 
-do gcloud compute tpus tpu-vm create ${sweeper_vm_prefix}_${index} \
+do gcloud compute tpus tpu-vm create ${swift_vm_prefix}_${index} \
     --project=${gcp_project_id} \
     --zone=${gcp_zone} \
     --accelerator-type=${gcp_accelerator_type} \
     --version=${gcp_tpu_vm_software_version} \
     --metadata startup-script='#! /bin/bash
         # Setup shared disk (read-only)
-        sudo mkdir -pv /mnt/sweeper
-        sudo mount -o discard,defaults /dev/sdb /mnt/sweeper
-        source /mnt/sweeper/.env
-        bash sweeper_setup.sh
+        sudo mkdir -pv /mnt/swift
+        sudo mount -o discard,defaults /dev/sdb /mnt/swift
+        source /mnt/swift/.env
+        bash swift_setup.sh
         bash sweep_launch.sh
         EOF'
 
