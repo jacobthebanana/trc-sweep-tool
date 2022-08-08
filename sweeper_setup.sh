e8df37b105b61860184511f3880cbe8346200beb
jacobthebanana <jacob@banana.abay.cf>
2022-08-08 20:49:30 +0000

    Included sudo in ssh tunneling script

--- sweeper_setup.sh
+++ sweeper_setup.sh
@@ -2,12 +2,11 @@
 
 # Copy project folder to local drive.
 cp -ar /mnt/sweeper/project ~/project
-ln -s /mnt/sweeper/data ~/project/data
 
 PYTHON=~/project/env/bin/python3
 
 # Setup wandb tunneling through SSH and authenticate.
-ssh -L 127.0.0.1:443:${wandb_api_host}:443 ${relay_login_user}@${relay_private_ip} &
+sudo ssh -L 127.0.0.1:443:${wandb_api_host}:443 ${relay_login_user}@${relay_private_ip} &
 echo "127.0.0.1 ${wandb_api_host}" | sudo tee -a /etc/hosts
 ${PYTHON3} -m wandb login --cloud ${wandb_api_key}
 
