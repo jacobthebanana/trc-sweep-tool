19f10e5e3bbf6ffc7ad3cfd036240702b0e3d7dc
jacobthebanana <jacob@banana.abay.cf>
2022-08-09 01:32:28 +0000

    Revised gcloud command.

--- gcloud_launch.sh
+++ gcloud_launch.sh
@@ -7,15 +7,18 @@ gcloud compute disks create ${destination_disk_name} \
 
 
 for index in {01..30} 
-do gcloud compute tpus tpu-vm create ${swift_vm_prefix}_${index} \
+do gcloud compute tpus tpu-vm create ${sweeper_vm_prefix}_${index} \
     --project=${gcp_project_id} \
     --zone=${gcp_zone} \
     --accelerator-type=${gcp_accelerator_type} \
     --version=${gcp_tpu_vm_software_version} \
+    --preemptible \ 
+    --internal-ips \
+    --subnetwork default \
+    --data-disk source=projects/${gcp_project_id}/zones/${gcp_zone}/disks/${destination_disk_name} \
     --metadata startup-script='#! /bin/bash
-        # Setup shared disk (read-only)
         sudo mkdir -pv /mnt/swift
-        sudo mount -o discard,defaults /dev/sdb /mnt/swift
+        sudo mount -o defaults /dev/sdb /mnt/swift
         source /mnt/swift/.env
         cd /mnt/swift
         bash sweeper_setup.sh
--- sweeper_launch.sh
+++ sweeper_launch.sh
@@ -2,5 +2,6 @@
 PYTHON=~/project/env/bin/python3
 cd ~/project/
 source ~/project/env/bin/activate
+export TRANSFORMERS_CACHE=/mnt/swift/cache
 export HF_DATASETS_CACHE="/dev/shm"
 ${PYTHON} -m wandb agent ${wandb_sweep}
